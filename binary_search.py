06fa288080e7c7fd91b1dd6674d28cd35d18ad54
Luis <mikeylopez@gmail.com>
2015-06-01 15:05:31 -0400

    fix non-recursive binary search

--- binary_search.py
+++ binary_search.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python
 
-def bin_search(lst, key, frm, to):
-    if not to:
-        to = len(lst)
-    if not frm:
-        frm = 0
-    mid = ((frm + to) / 2) 
-    if key > lst[mid]:
-        bin_search(lst, key, lst[mid]+1, len(lst))
-    elif key < lst[mid]:
-        bin_search(lst, key, 0, lst[mid]-1)
+import sys
+
+def bin_search(lst, key, first, last):
+    if first > last:
+        return False
+    index = (first + last) / 2
+    print "index is {0}".format(index)
+    if key == lst[index]:
+        print "found it: {0}".format(index)
+        return True 
+    if key < lst[index]:
+        print "key is less {0}".format(index)
+        bin_search(lst, key, first, index - 1)
     else:
-        print "found it => {0}".format(key) 
+        print "key is more {0}".format(index)
+        bin_search(lst, key, index + 1, last)
+    return index
+
+def bin_search_iter(lst, key, first, last):
+    if not last:
+        last = len(lst)
+    while True:
+        if first > last:
+            return False
+        index = (first + last) / 2
+        if lst[index] == key:
+            print "found key @ index {0}".format(lst[index])
+            return
+        elif key < lst[index]:
+           print "key is less, diff 1 on index @ {0}".format(lst[index])
+           last = lst[index] - 1
+        else:
+           print "key is more, plus 1 to index @ {0}".format(lst[index])
+           first = lst[index] + 1
 
 
-lst = range(0,20)
-key = 15 
-bin_search(lst, key, 0, len(lst))
+lst = range(0, 100000)
+key = 10 
+print "key is {0}".format(key)
+bin_search_iter(lst, key, 0, len(lst))
